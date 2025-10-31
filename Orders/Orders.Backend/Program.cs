60dea6411d5f22c22a4a16234df545d834a58436
desarrollopracticas <desarrollopracticas@DESKTOP-F15BGSN>
2025-10-31 11:51:09 -0500

    Add basic countries api controller

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,18 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name= LocalConnection"));
 
 var app = builder.Build();
 
+app.UseCors(x => x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowed(origin => true)
+.AllowCredentials());
 
 if (app.Environment.IsDevelopment())
 {
